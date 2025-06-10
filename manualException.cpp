711edbebfa4a2f2d31d5f24649fed664fe97e4fd
Lenovo <9.3razzinayyuri@gmail.com>
2025-06-10 18:46:28 +0700

    manual exception melempar interger

---
+++ manualException.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try{
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        throw 0.5 //melemparkan sebuah interger maka
+        cout << "Pernyataan ini tidak akan di eksekusi" << endl;
+    }
+   
+}
