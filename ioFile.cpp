35f5cd42c320186f2000a0407d8b011f452162e4
Lenovo <9.3razzinayyuri@gmail.com>
2025-06-10 17:33:33 +0700

    jika memasikkan huruf q maka otomatis codingan akan berhenti

--- ioFile.cpp
+++ ioFile.cpp
@@ -12,5 +12,14 @@ int main() {
     cout << "> Menulis File, \'q\' untuk keluar" << endl;
     //unlimited loop untuk menulis
 
-   
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai 'baris' ke dalam file 
+        outfile << baris << endl;
+    }
+    
 }
