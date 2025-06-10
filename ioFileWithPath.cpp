4e444a473840f65382f36985281ecb896234572b
Lenovo <9.3razzinayyuri@gmail.com>
2025-06-10 18:28:16 +0700

    refisi

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -31,7 +31,7 @@ int main (){
     ifstream infile;
     //menunjuk ke sebuah nama file
     infile.open(NamaFile, ios::in);
-    cout << endl; << ">= membuka dan membaca file " << endl;
+    cout << ">= membuka dan membaca file " << endl;
     //jika file ada maka
     if (infile.is_open())
     {
